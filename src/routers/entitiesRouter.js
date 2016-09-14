474f408c930e6c1bc9e78cbb28d2d9420ad8baf2
sheldar@wp.pl <sheldar@wp.pl>
2016-09-14 23:28:06 +0200

    Beta version for endpoint for creating entities

--- src/routers/entitiesRouter.js
+++ src/routers/entitiesRouter.js
@@ -16,6 +16,47 @@ module.exports = function (io) {
         });
     });
 
+    // entitiesRouter.post('/entity', function (request, response, next) {
+    //     fs.readFile('./var/data/entities.json', 'utf8', function (error, data) {
+    //         if (error) {
+    //             return next(error);
+    //         }
+    //
+    //         if(!request.body.entityName) {
+    //             response.sendStatus(400);
+    //             return;
+    //         }
+    //
+    //         var entities = JSON.parse(data);
+    //
+    //         var nextId = Math.max.apply(Math, entities.map(function(entity) {
+    //             return entity.id;
+    //         })) + 1;
+    //
+    //         var entity = {
+    //             "id": nextId,
+    //             "lastModificationDate": "",
+    //             "lastModifiedById": 0,
+    //             "name": request.body.entityName,
+    //             "locked": false
+    //         };
+    //
+    //         entities.push(entity);
+    //
+    //         fs.writeFile('./var/data/entities.json', JSON.stringify(entities));
+    //
+    //         var eventName = entity.locked ? 'lock' : 'unlock';
+    //
+    //         io.sockets.emit(eventName, {
+    //             entityName: entity.name,
+    //             modifiedById: request.body.lastModifiedById,
+    //             modificationDate: request.body.lastModificationDate
+    //         });
+    //
+    //         response.sendStatus(200);
+    //     });
+    // });
+
     entitiesRouter.put('/entity/:entityId', function (request, response, next) {
         fs.readFile('./var/data/entities.json', 'utf8', function (error, data) {
             if (error) {
